f0ef9d9e5da4b8ef91aa2e0b7599a28abf7ee4b1
davidherdu <david.hernandez.duran@gmail.com>
2023-04-22 16:09:08 +0300

    Formatting of PriceNotFoundException file

--- src/main/java/com/david/zara/prices/exception/PriceNotFoundException.java
+++ src/main/java/com/david/zara/prices/exception/PriceNotFoundException.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 public class PriceNotFoundException extends RuntimeException {
 	
-	public PriceNotFoundException(LocalDateTime appDate, Long productId, Long brandId) {
+    public PriceNotFoundException(LocalDateTime appDate, Long productId, Long brandId) {
         super(String.format("Price for productId %s, brandId %s at %s not found : ", productId, brandId, appDate));
     }
 }
