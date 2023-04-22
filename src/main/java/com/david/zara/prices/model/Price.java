178d8f28fabb17fd8b750d33002cc086e56cd85f
davidherdu <david.hernandez.duran@gmail.com>
2023-04-22 16:08:28 +0300

    Formatting of Price file

--- src/main/java/com/david/zara/prices/model/Price.java
+++ src/main/java/com/david/zara/prices/model/Price.java
@@ -15,30 +15,30 @@ import lombok.NoArgsConstructor;
 @Table(name = "prices") 
 public class Price {
 	
-	@Id @GeneratedValue @Column(name = "id")
+    @Id @GeneratedValue @Column(name = "id")
     private Long id;
-	
+
     @Column(name = "brand_id")
     private Long brandId;
-	
+
     @Column(name = "start_date")
     private LocalDateTime startDate;
-	
+
     @Column(name = "end_date")
     private LocalDateTime endDate;
-	
+
     @Column(name = "price_list")
     private Long priceList;
-	
+
     @Column(name = "product_id")
     private Long productId;
-	
+
     @Column(name = "priority")
     private Integer priority;
-	
+
     @Column(name = "price")
     private Double price;
-	
+
     @Column(name = "currency")
     private String currency;
 }
